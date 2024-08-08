a5ecff187ed0e0f7b2b95cbfee5eb90af369a2a0
rb5883 <rb58853@gmail.com>
2024-08-08 18:04:55 -0400

    save

--- src/environment/env.jsx
+++ src/environment/env.jsx
@@ -1,8 +1,8 @@
 const env = {
     boxProjectMinWidth: 300,
     boxProjectMinWidthMobile: 185,
-    mode: 'light',
-    flagColors:['rgb(143, 72, 72)',' rgb(81, 121, 87)', 'rgb(65, 45, 136)', ' rgb(206, 180, 36)']
+    mode: 'dark',
+    flagColors: ['rgb(143, 72, 72)', ' rgb(81, 121, 87)', 'rgb(65, 45, 136)', ' rgb(206, 180, 36)']
 }
 
 export default env
