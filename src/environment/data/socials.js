5b0b49e24b9cefed74ba6f6c6ab2ff105dc84789
rb58853 <rb58853@gmail.com>
2025-05-27 02:30:37 -0400

    Arcane Triler

--- src/environment/data/socials.js
+++ src/environment/data/socials.js
@@ -2,7 +2,7 @@ const socials = {
     github: 'https://github.com/rb58853',
     gmail: 'mailto:rb58853@gmail.com',
     telegram: 'https://t.me/rb58853',
-    cv: 'https://github.com/rb58853/rb58853/raw/main/assets/cv%20.pdf',
+    cv: 'https://github.com/rb58853/rb58853/raw/main/assets/cv.pdf',
     loc: 'https://www.google.com/maps/place/Havana/data=!4m2!3m1!1s0x88cd774cbdf600e1:0x4e294d7e1a770d60?sa=X&ved=1t:242&ictx=111'
     // linkedIn: 'https://www.linkedin.com/in/ra%C3%BAl-beltr%C3%A1n-g%C3%B3mez-44700a303?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app',
     // discord: 'https://t.me/rb58853',
